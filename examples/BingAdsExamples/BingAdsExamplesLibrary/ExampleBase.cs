4f50b4aafec351ab235a19332f3ccbaa9517a581
Eric Urban <eric.urban@microsoft.com>
2016-07-01 07:26:25 -0700

    patch for v9 sunset

--- examples/BingAdsExamples/BingAdsExamplesLibrary/ExampleBase.cs
+++ examples/BingAdsExamples/BingAdsExamplesLibrary/ExampleBase.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Threading.Tasks;
 using Microsoft.BingAds;
-using Microsoft.BingAds.CampaignManagement;
 
 namespace BingAdsExamplesLibrary
 {
