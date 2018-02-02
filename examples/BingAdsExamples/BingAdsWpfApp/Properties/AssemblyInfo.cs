5c9b6f4958736f5c7f00faa0ba65a797f8266179
Eric Urban <eur@microsoft.com>
2018-02-02 13:46:53 -0800

    assembly info 2018

--- examples/BingAdsExamples/BingAdsWpfApp/Properties/AssemblyInfo.cs
+++ examples/BingAdsExamples/BingAdsWpfApp/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BingAdsWpfApp")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
